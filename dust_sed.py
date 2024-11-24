af1e1ca942ef745254cafb2c271d2bd9bf594894
ScorP1onnn <saimuralikolupuri@gmail.com>
2024-11-24 20:38:16 +0530

    Added SED for J2310+1855

--- dust_sed.py
+++ dust_sed.py
@@ -12,8 +12,8 @@ import astropy.units as u
 import emcee
 from scipy.signal import freqs
 from uncertainties import ufloat
-
 import utils as utils
+
 
 """
 
@@ -166,6 +166,7 @@ sai_wave = utils.ghz_to_mum(sai_freq)
 sai_flux = ufloat(0.8,0.1) #lux in mJy
 
 
+#Plot SED
 plt.scatter(wave_ned,flux_ned,label='SDSS-NED')
 plt.scatter(w1_wave,w1_flux.n,label='W1-Blain et al. 2013')
 plt.scatter(leipski_wave,leipski_flux,label='Leipski et al. 2014')
@@ -173,13 +174,130 @@ plt.scatter(wwt_wave,wwt_flux,label='WWT')
 plt.scatter(hash_wave,hash_flux.n,label='Hashimoto et al. 2018')
 plt.scatter(salak_wave,salak_flux.n,label='Salak et al. 2024')
 
-plt.scatter(sai_wave,sai_flux.n,label='Our Value',marker='*',s=90,color='blue')
+plt.scatter(sai_wave,sai_flux.n,label='Our Value',marker='*',s=120,color='blue')
 
 plt.xscale('log')
 plt.yscale('log')
-#plt.axvline(205*(1+6.0391))
 plt.xlabel("Observed Wavelength")
 plt.ylabel("Flux Density (mJy)")
 plt.legend()
 plt.show()
 
+
+
+
+
+
+
+
+###################################################################################################
+
+#J2310+1855
+
+
+
+#PAN-STARR from NED (Check the reference paper on that wesbite)
+"""
+freq_ned = np.array([3.97e+14, 3.45e+14, 3.15e+14])/1e9 #Convert to GHz
+wave_ned = utils.ghz_to_mum(freq_ned)
+flux_ned = np.array([0.00000871, 0.0000506, 8.47e-5]) * 1e3 #Convert t0 mJy
+flux_err_ned = np.array([8.02e-7, 1.86e-6, 3.12e-6]) * 1e3 #Convert t0 mJy
+
+"""
+
+freq_ned = np.array([3.97e+14])/1e9 #Convert to GHz
+wave_ned = utils.ghz_to_mum(freq_ned)
+flux_ned = np.array([0.00000871]) * 1e3 #Convert t0 mJy
+flux_err_ned = np.array([8.02e-7]) * 1e3 #Convert t0 mJy
+
+
+
+
+#Ross & Cross, 2019 (https://arxiv.org/abs/1906.06974v1)
+df = pd.read_csv("/home/sai/Downloads/VHzQs_ZYJHK_WISE_v3.dat",delimiter='\t')
+#print(df.columns)
+
+filtered_row = df[df['desig'] == 'J2310+1855']  # Ensure '2310+1855' matches the data type and formatting in the DataFrame
+#print(filtered_row[['unW1mag', 'unW1err','unW2mag', 'unW2err','w3mpro', 'w3sig']])
+
+
+W1_mag = ufloat(float(filtered_row['unW1mag'].values), float(filtered_row['unW1err'].values))
+W2_mag = ufloat(float(filtered_row['unW2mag'].values), float(filtered_row['unW2err'].values))
+W3_mag = ufloat(float(filtered_row['w3mpro'].values), float(filtered_row['w3sig'].values))
+
+W1_mjy = 309.540 * 10**(W1_mag/-2.5) * 1e3 #Convert to mJy
+W2_mjy = 171.787 * 10**(W2_mag/-2.5) * 1e3 #Convert to mJy
+W3_mjy = 31.674 * 10**(W3_mag/-2.5) * 1e3 #Convert to mJy
+
+wise_freq = np.array([8.94e+13,6.51e+13, 2.59e+13])/1e9 #Convert to GHz
+wise_wave = utils.ghz_to_mum(wise_freq) #Convert to micrometer
+wise_flux = np.array([W1_mjy.n,W2_mjy.n,W3_mjy.n])
+wise_flux_err = np.array([W1_mjy.s,W2_mjy.s,W3_mjy.s])
+
+print(W1_mjy,W2_mjy,W3_mjy)
+
+
+#Wang et al. 2008
+wang_freq = np.array([99,250]) # IN GHz
+wang_wave = utils.ghz_to_mum(wang_freq) #convert to micrometer
+wang_flux = np.array([0.4, 8.29]) #in mJy
+wang_flux_err = np.array([0.05,0.63])#in mJy
+
+
+#Hashimoto et al. 2018 (https://arxiv.org/pdf/1811.00030)
+#Our data probe dust continuum emission at the rest-frame wavelength, λ_rest, of ≈ 87 μm
+hash_wave = np.array([87*(1+6.0035)]) #In micrometers
+hash_freq = utils.mum_to_ghz(hash_wave)
+hash_flux = ufloat(24.89,0.21) #Flux in mJy
+
+
+#Tripodi et al. 2022 (https://www.aanda.org/articles/aa/pdf/2022/09/aa43920-22.pdf)
+tripodi_freq = np.array([91.5, 136.627, 140.995, 153.07, 263.315, 265.369, 284.988, 289.18, 344.185, 490.787]) # IN GHz
+tripodi_wave = utils.ghz_to_mum(tripodi_freq) #convert to micrometer
+tripodi_flux = np.array([0.29,1.29,1.40, 1.63, 7.73, 8.81, 11.05, 11.77, 14.63, 25.31]) #in mJy
+tripodi_flux_err = np.array([0.01, 0.03, 0.02, 0.06, 0.31, 0.13, 0.16, 0.12, 0.34, 0.19]) #in mJy
+
+
+#Shao, Y., Wang, R., Carilli, C. L., et al. 2019, ApJ, 876, 99
+#Herschel SPIRE and PACS
+shao_freq = np.array([131, 135, 143,147,283, 295, 338, 350, 856.549, 1199.169 , 1873.703, 2997.924]) # IN GHz
+shao_wave = utils.ghz_to_mum(shao_freq) #convert to micrometer
+shao_flux = np.array([1.10, 1.24,1.41,1.59, 11.60, 11.56, 14.49, 14.54, 22, 19.9, 13.2, 6.5]) #in mJy
+shao_flux_err = np.array([0.05, 0.04, 0.04, 0.04, 0.1,0.1,0.21,0.21, 6.9, 6.0, 2.8, 1.2])
+
+
+
+
+
+#Butler et al. 2023 (https://iopscience.iop.org/article/10.3847/1538-4357/acad03/pdf)
+#λ_rest, of ≈ 119 μm
+butler_wave = np.array([119 * (1+6.0035)])
+butler_freq = utils.mum_to_ghz(butler_wave)
+butler_flux = ufloat(4.73, 0.0016) #in mJy
+
+
+
+#Our Value
+sai_freq = np.array([1461.134/(1+6.0035)]) #in GHz
+sai_wave = utils.ghz_to_mum(sai_freq)
+sai_flux = ufloat(5.5,0.8) #lux in mJy
+
+
+
+
+plt.scatter(wave_ned,flux_ned,label='NED')
+plt.scatter(wise_wave,wise_flux,label='WISE')
+plt.scatter(wang_wave,wang_flux,label='Wang et al. 2008')
+plt.scatter(hash_wave,hash_flux.n,label='Hashimoto et al. 2018')
+plt.scatter(tripodi_wave,tripodi_flux,label='Tripodi et al. 2022')
+plt.scatter(shao_wave,shao_flux,label='Shao et al. 2019')
+plt.scatter(butler_wave,butler_flux.n,label='Butler et al. 2023')
+
+plt.scatter(sai_wave,sai_flux.n,label='Our Value',marker='*',s=120,color='blue')
+
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel("Observed Wavelength")
+plt.ylabel("Flux Density (mJy)")
+plt.legend()
+plt.show()
