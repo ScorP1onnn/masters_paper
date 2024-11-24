ec93f9e99e504972604d09446a76a5a1418c91fa
ScorP1onnn <saimuralikolupuri@gmail.com>
2024-11-24 22:18:19 +0530

    Added SED for PSSJ2322+1944

--- dust_sed.py
+++ dust_sed.py
@@ -101,7 +101,9 @@ def plot_sed_wave(wave,fluxes,wave_extra = [],fluxes_extra = []):
     plt.show()
 
 
+############################################################################################
 
+#J2054-0005
 
 #SDSS from NED
 df = pd.read_csv(r'NED_photo_points/sdss_j2054_0005_table_photandseds_NED.csv')
@@ -178,8 +180,9 @@ plt.scatter(sai_wave,sai_flux.n,label='Our Value',marker='*',s=120,color='blue')
 
 plt.xscale('log')
 plt.yscale('log')
-plt.xlabel("Observed Wavelength")
-plt.ylabel("Flux Density (mJy)")
+plt.xlabel(r"Observed Wavelength [$\mu$m]")
+plt.ylabel("Flux Density [mJy]")
+plt.title("J2054-0005")
 plt.legend()
 plt.show()
 
@@ -214,7 +217,7 @@ flux_err_ned = np.array([8.02e-7]) * 1e3 #Convert t0 mJy
 
 
 #Ross & Cross, 2019 (https://arxiv.org/abs/1906.06974v1)
-df = pd.read_csv("/home/sai/Downloads/VHzQs_ZYJHK_WISE_v3.dat",delimiter='\t')
+df = pd.read_csv("NED_photo_points/VHzQs_ZYJHK_WISE_v3.dat",delimiter='\t')
 #print(df.columns)
 
 filtered_row = df[df['desig'] == 'J2310+1855']  # Ensure '2310+1855' matches the data type and formatting in the DataFrame
@@ -258,29 +261,38 @@ tripodi_flux = np.array([0.29,1.29,1.40, 1.63, 7.73, 8.81, 11.05, 11.77, 14.63, 
 tripodi_flux_err = np.array([0.01, 0.03, 0.02, 0.06, 0.31, 0.13, 0.16, 0.12, 0.34, 0.19]) #in mJy
 
 
+
+"""
 #Shao, Y., Wang, R., Carilli, C. L., et al. 2019, ApJ, 876, 99
 #Herschel SPIRE and PACS
 shao_freq = np.array([131, 135, 143,147,283, 295, 338, 350, 856.549, 1199.169 , 1873.703, 2997.924]) # IN GHz
 shao_wave = utils.ghz_to_mum(shao_freq) #convert to micrometer
 shao_flux = np.array([1.10, 1.24,1.41,1.59, 11.60, 11.56, 14.49, 14.54, 22, 19.9, 13.2, 6.5]) #in mJy
 shao_flux_err = np.array([0.05, 0.04, 0.04, 0.04, 0.1,0.1,0.21,0.21, 6.9, 6.0, 2.8, 1.2])
+"""
+
+#Shao, Y., Wang, R., Carilli, C. L., et al. 2019, ApJ, 876, 99
+#Herschel SPIRE and PACS
+shao_freq = np.array([856.549, 1199.169 , 1873.703, 2997.924]) # IN GHz
+shao_wave = utils.ghz_to_mum(shao_freq) #convert to micrometer
+shao_flux = np.array([ 22, 19.9, 13.2, 6.5]) #in mJy
+shao_flux_err = np.array([ 6.9, 6.0, 2.8, 1.2])
 
 
 
-
-
+"""
 #Butler et al. 2023 (https://iopscience.iop.org/article/10.3847/1538-4357/acad03/pdf)
 #λ_rest, of ≈ 119 μm
 butler_wave = np.array([119 * (1+6.0035)])
 butler_freq = utils.mum_to_ghz(butler_wave)
 butler_flux = ufloat(4.73, 0.0016) #in mJy
-
+"""
 
 
 #Our Value
 sai_freq = np.array([1461.134/(1+6.0035)]) #in GHz
 sai_wave = utils.ghz_to_mum(sai_freq)
-sai_flux = ufloat(5.5,0.8) #lux in mJy
+sai_flux = ufloat(5.5,0.8) #flux in mJy
 
 
 
@@ -291,13 +303,50 @@ plt.scatter(wang_wave,wang_flux,label='Wang et al. 2008')
 plt.scatter(hash_wave,hash_flux.n,label='Hashimoto et al. 2018')
 plt.scatter(tripodi_wave,tripodi_flux,label='Tripodi et al. 2022')
 plt.scatter(shao_wave,shao_flux,label='Shao et al. 2019')
-plt.scatter(butler_wave,butler_flux.n,label='Butler et al. 2023')
+#plt.scatter(butler_wave,butler_flux.n,label='Butler et al. 2023')
 
 plt.scatter(sai_wave,sai_flux.n,label='Our Value',marker='*',s=120,color='blue')
 
 plt.xscale('log')
 plt.yscale('log')
-plt.xlabel("Observed Wavelength")
-plt.ylabel("Flux Density (mJy)")
+plt.xlabel(r"Observed Wavelength [$\mu$m]")
+plt.ylabel("Flux Density [mJy]")
+plt.title("J2310-1855")
 plt.legend()
 plt.show()
+
+
+
+
+
+##################################################################################################
+
+
+#PSSJ2322+1944
+#Last two values are from Stacey et al. 2018 (https://ui.adsabs.harvard.edu/abs/2018MNRAS.476.5075S/abstract)
+
+j2322_freq = np.array([1.4,90, 201.78,225,231, 353, 660, 856.54, 1875, 4300]) #IN GHz
+j2322_wave = utils.ghz_to_mum(j2322_freq)
+j2322_flux = np.array([9.8e-5, 0.4e-3, 5.79e-3,0.0075,0.0096,0.0225,0.075 , 79e-3, 0.0434, 0.0137]) *1e3 #Converting to mJy
+j2322_flux_err = np.array([1.5e-5, 0.25e-3, 0.77e-3,0.0013, 0.0005, 0.0025, 0.019, 11e-3, 0.0084, 0.0061]) *1e3 #Converting to mJy
+
+
+#Our Value
+j2322_sai_freq = np.array([1461.134/(1+4.12)]) #in GHz
+j2322_sai_wave = utils.ghz_to_mum(sai_freq)
+j2322_sai_flux = ufloat(17.4,2.6) #flux in mJy
+
+
+plt.scatter(j2322_wave,j2322_flux)
+#plt.scatter(utils.ghz_to_mum(96),0.31) #Ignore this point
+plt.scatter(j2322_sai_wave,j2322_sai_flux.n,label='Our Value',marker='*',s=120,color='blue')
+
+
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel(r"Observed Wavelength [$\mu$m]")
+plt.ylabel("Flux Density [mJy]")
+plt.title("PSSJ2322+1944")
+plt.legend()
+plt.show()
+
