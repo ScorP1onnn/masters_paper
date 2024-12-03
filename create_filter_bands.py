cb4c6b68a07d18db54655f001a19365c7cc16551
ScorP1onnn <saimuralikolupuri@gmail.com>
2024-12-03 14:09:42 +0530

    Tripodi+24 bands

--- create_filter_bands.py
+++ create_filter_bands.py
@@ -12,7 +12,7 @@ import utils as utils
 from utils import ghz_to_mum
 
 
-def alma_filter(rest_freq,z,file_name,tuning_freq=[],middle_freq=0,alma_bandwidth=7.5,bin=30,plot=False):
+def alma_filter(file_name,rest_freq=0.,z=0.,tuning_freq=[],middle_freq=0.,alma_bandwidth=7.5,bin=30.,plot=False):
 
 
     if middle_freq==0:
@@ -22,7 +22,12 @@ def alma_filter(rest_freq,z,file_name,tuning_freq=[],middle_freq=0,alma_bandwidt
         central_freq = middle_freq
 
 
-    ref_freq = rest_freq/(1+z)
+    if rest_freq!=0 and z!=0:
+        ref_freq = rest_freq / (1 + z)
+    else:
+        ref_freq = middle_freq
+
+    #ref_freq = rest_freq/(1+z)
     freq_Step = tools.kms2ghz(bin,ref_freq)
 
     a = central_freq - ((alma_bandwidth / 2) - freq_Step)
@@ -39,15 +44,46 @@ def alma_filter(rest_freq,z,file_name,tuning_freq=[],middle_freq=0,alma_bandwidt
     #print(ghz_to_mum(array_GHz.value))
 
 
-    #Wavelength in Angstroms in observed frame (or sky frame)
+
+
+
+
+    #print(transmission)
+    transmission_split = np.array_split(transmission[1:-1],60)
+    array_angstroms_split = np.array_split(array_angstroms[1:-1].value,60)
+
+    transmission_avg = np.array([np.mean(bin) for bin in transmission_split])
+    array_angstroms_avg = np.array([np.mean(bin) for bin in array_angstroms_split])
+
+    #for i in range(len(a)):
+        #print(len(a[i]),len(b[i]))
+
+
+
+
+
+    transmission_avg = np.insert(transmission_avg,0,transmission[0])
+    transmission_avg = np.insert(transmission_avg,len(transmission_avg),transmission[len(transmission) - 1])
+
+    array_angstroms_avg = np.insert(array_angstroms_avg, 0, array_angstroms[0].value)
+    array_angstroms_avg = np.insert(array_angstroms_avg, len(array_angstroms_avg), array_angstroms[len(array_angstroms) - 1].value)
+
+
+    transmission = transmission_avg
+    array_angstroms = array_angstroms_avg
+    array_GHz = (array_angstroms * u.angstrom).to(u.GHz, equivalencies=u.spectral())
+
+
+    # Wavelength in Angstroms in observed frame (or sky frame)
     desktop_path = os.path.expanduser('~') + '/Desktop' + '/alma_iram_filters'
     file_path = os.path.join(desktop_path, file_name + '.dat')
     file_1 = open(file_path, "w+")
     file_1.write(f"# {file_name}\n# energy\n")
-    file_1.write(f"# boxcar shape filter centered at {central_freq} GHz or {utils.ghz_to_mum(central_freq) *1e4} Angstroms (or {np.round(utils.ghz_to_mum(central_freq),2)} micrometer)")
+    file_1.write(
+        f"# boxcar shape filter centered at {central_freq} GHz or {utils.ghz_to_mum(central_freq) * 1e4} Angstroms (or {np.round(utils.ghz_to_mum(central_freq), 2)} micrometer)")
     for i in range(len(array_angstroms)):
         file_1.write(f"\n")
-        file_1.write(f"{np.round(array_angstroms[i].value,4)} {transmission[i]}")
+        file_1.write(f"{np.round(array_angstroms[i], 4)} {transmission[i]}")
     file_1.close()
     print(f"FILE {file_name + '.dat'} CREATED")
 
@@ -55,7 +91,10 @@ def alma_filter(rest_freq,z,file_name,tuning_freq=[],middle_freq=0,alma_bandwidt
 
 
 
+
     #Wavelength in Observed Frame (or Sky frame)
+    #The vertical line is a bit off from the actual GHz axis but is exactly correct in the wavlength axis (i.e. when
+    #you convert the GHz value to angstrom). The only thing that matters is that the value is within the profile
     if plot == True:
 
         fig,ax = plt.subplots(1,1,tight_layout=True)
@@ -88,7 +127,7 @@ def alma_filter(rest_freq,z,file_name,tuning_freq=[],middle_freq=0,alma_bandwidt
 
 
 
-
+"""
 #Hashimoto et al. 2019
 restfreq = 3393.006244
 z_j2054=6.0391
@@ -106,3 +145,11 @@ restfreq = 2437.3370569 #for rest wavelength of 123microns
 z_j2054=6.0391
 file_salak = 'salak_j2054'
 alma_filter(rest_freq=restfreq,z=z_j2054,file_name=file_salak,tuning_freq=[344.2,346.1],alma_bandwidth=8,bin=35,plot=True)
+"""
+
+
+#Tripodi et al. 2024
+obs_freq_92 = 92.26 #Use it as middle freq
+file_tripodi_2024_92 = 'tripodi_2024_92_GHz'
+alma_filter(file_name=file_tripodi_2024_92,middle_freq=obs_freq_92,bin=50,plot=True)
+
